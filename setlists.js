6067f279efac88436bc2f3d043d1305b6bdbe5dc
masanorisato04131031 <sato.ma@vantan.co.jp>
2026-05-06 21:26:02 +0900

    Update setlists.js

--- setlists.js
+++ setlists.js
@@ -74,4 +74,115 @@ window.setlists = {
 ]}
 ]
 
+// ===== 1994 =====
+setlists["1994"] = [
+  {
+    title: "CHAGE&ASKA『ASIAN TOUR 1994』（全5公演）",
+    songs: [
+      "夜明けは沈黙のなかへ",
+      "なぜに君は帰らない",
+      "GUYS",
+      "CATCH & RELEASE",
+      "HANG UP THE PHONE",
+      "LOVE SONG",
+      "はじまりはいつも雨",
+      "終章",
+      "男と女",
+      "You are free",
+      "今夜ちょっとさ",
+      "Sons and Daughters～それより僕が伝えたいのは",
+      "RED HILL",
+      "Mr.ASIA",
+      "SAY YES",
+      "Mr.Jの悲劇は岩より重い",
+      "僕はこの瞳で嘘をつく",
+      "YAH YAH YAH",
+      "ロマンシングヤード",
+      "WALK",
+      "BIG TREE"
+    ]
+  },
+  {
+    title: "MULTIMAX『well,well,well』（全16公演）",
+    songs: [
+      "Well,Well,Well",
+      "SO SO",
+      "勇気の言葉",
+      "いいんじゃあないのォ",
+      "島の娘",
+      "Hello!",
+      "MUSIC",
+      "ひらひら",
+      "KIGEN GA WARUINO",
+      "MILKY WAY BLUES",
+      "SOME DAY",
+      "月が言い訳をしてる",
+      "愛の空で",
+      "愛がお前をすくい投げ",
+      "WORKING",
+      "DEEP",
+      "組曲WANDERING",
+      "BEATLESメドレー",
+      "WINDY ROAD"
+    ]
+  }
+];
+
+// ===== 1995 =====
+setlists["1995"] = [
+  {
+    title: "CHAGE&ASKA『CONCERT TOUR'95～'96 SUPER BEST 3 MISSION IMPOSSIBLE』（1+60公演）",
+    songs: [
+      "HEART",
+      "Love Affair",
+      "BROTHER",
+      "ある晴れた金曜日の朝",
+      "HANG UP THE PHONE",
+      "SAY YES",
+      "NとLの野球帽",
+      "紫陽花と向日葵",
+      "男と女",
+      "何日君再来",
+      "river",
+      "PRIDE",
+      "On Your Mark",
+      "Something There",
+      "Mr.ASIA",
+      "モナリザの背中よりも",
+      "僕はこの瞳で嘘をつく",
+      "YAH YAH YAH",
+      "can do now",
+      "ロマンシングヤード",
+      "LOVE SONG",
+      "On Your Mark"
+    ]
+  },
+  {
+    title: "CHAGE&ASKA『ASIAN TOUR MISSION IMPOSSIBLE』（5公演）",
+    songs: [
+      "HEART",
+      "Love Affair",
+      "BROTHER",
+      "ある晴れた金曜日の朝",
+      "HANG UP THE PHONE",
+      "SAY YES",
+      "You are free",
+      "紫陽花と向日葵",
+      "男と女",
+      "何日君再来",
+      "めぐり逢い",
+      "PRIDE",
+      "On Your Mark",
+      "Something There",
+      "Mr.ASIA",
+      "モナリザの背中よりも",
+      "僕はこの瞳で嘘をつく",
+      "YAH YAH YAH",
+      "can do now",
+      "ロマンシングヤード",
+      "LOVE SONG",
+      "On Your Mark"
+    ]
+  }
+];  
 };
